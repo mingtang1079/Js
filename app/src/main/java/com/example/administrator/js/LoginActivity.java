6321373b6d0e583062d17aac5281c3a4d252425b
tangming <1079240451@qq.com>
2018-04-04 18:06:08 +0800

    login test

--- app/src/main/java/com/example/administrator/js/LoginActivity.java
+++ app/src/main/java/com/example/administrator/js/LoginActivity.java
@@ -2,12 +2,12 @@ package com.example.administrator.js;
 
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.text.style.TtsSpan;
 import android.util.Log;
 import android.view.View;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 
 import com.appbaselib.base.BaseActivity;
 import com.appbaselib.network.ResponceSubscriber;
@@ -23,7 +23,14 @@ import butterknife.OnClick;
 public class LoginActivity extends BaseActivity {
 
 
+    @BindView(R.id.tv_phone)
+    AutoCompleteTextView mTvPhone;
+    @BindView(R.id.password)
+    EditText mPassword;
+    @BindView(R.id.bt_sure)
     Button mBtSure;
+    @BindView(R.id.email_login_form)
+    LinearLayout mEmailLoginForm;
 
     @Override
     public Toolbar getToolbar() {
@@ -33,7 +40,7 @@ public class LoginActivity extends BaseActivity {
     @Override
     protected void initView() {
 
-        mBtSure=findViewById(R.id.bt_sure);
+        mBtSure = findViewById(R.id.bt_sure);
         mBtSure.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View mView) {
@@ -53,10 +60,11 @@ public class LoginActivity extends BaseActivity {
         return null;
     }
 
+    @OnClick(R.id.bt_sure)
     public void onViewClicked() {
 
         Http.getDefault().login("18202820092", "123456")
-                .compose(RxHelper.<User>handleResult())
+                .as(RxHelper.<User>handleResult(this))
                 .subscribe(new ResponceSubscriber<User>() {
                     @Override
                     protected void onSucess(User mUser) {
