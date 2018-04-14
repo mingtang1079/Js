4cff31165994e2c2185545787d7e7c86ce217217
tangming <1079240451@qq.com>
2018-04-14 21:45:50 +0800

    个人信息

---
+++ app/src/main/java/com/example/administrator/js/me/model/User.java
@@ -0,0 +1,69 @@
+package com.example.administrator.js.me.model;
+
+public class User {
+
+
+    /**
+     * id : 3cc8414eb37040f3854b9456b72aa17c
+     * isNewRecord : false
+     * remarks : null
+     * createDate : 2018-04-10 03:25:42
+     * updateDate : 2018-04-10 03:25:42
+     * realname : null
+     * nickname : null
+     * no : null
+     * openid : null
+     * editnum : 2
+     * password :
+     * comfirmpsd : null
+     * img : null
+     * age : null
+     * birthdate : null
+     * sex : null
+     * email : null
+     * address : null
+     * areacode : null
+     * areaname : null
+     * score : null
+     * degree : null
+     * intro : null
+     * role : 0
+     * deviceid : null
+     * beginBirthdate : null
+     * endBirthdate : null
+     * mobile : 18202820092
+     * longitude : null
+     * latitude : null
+     */
+
+    public String id;
+    public boolean isNewRecord;
+    public Object remarks;
+    public String createDate;
+    public String updateDate;
+    public String realname;
+    public String nickname;
+    public String no;
+    public String openid;
+    public int editnum;
+    public String password;
+    public String comfirmpsd;
+    public String img;
+    public String age;
+    public String birthdate;
+    public String sex;
+    public String email;
+    public String address;
+    public String areacode;
+    public String areaname;
+    public String score;
+    public String degree;
+    public String intro;
+    public String role;
+    public String deviceid;
+    public String beginBirthdate;
+    public String endBirthdate;
+    public String mobile;
+    public Long longitude;
+    public Long latitude;
+}
