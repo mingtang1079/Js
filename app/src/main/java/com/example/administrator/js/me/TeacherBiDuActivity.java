d09b23e6c2debe190edcaa7b8021aacb074b0630
tangming <1079240451@qq.com>
2018-05-28 00:06:15 +0800

    必读

--- app/src/main/java/com/example/administrator/js/me/TeacherBiDuActivity.java
+++ app/src/main/java/com/example/administrator/js/me/TeacherBiDuActivity.java
@@ -47,9 +47,18 @@ public class TeacherBiDuActivity extends BaseActivity {
 
     }
 
-    @OnClick({R.id.ll_zhize, R.id.ll_my_fuwu, R.id.ll_pingjia})
+    @OnClick({R.id.ll_zhize, R.id.ll_my_fuwu, R.id.ll_pingjia,R.id.ll_xuzhi})
     public void onViewClicked(View view) {
         switch (view.getId()) {
+
+            case  R.id.ll_xuzhi:
+
+                ARouter.getInstance().build("/web/Html5Activity")
+                        .withString("url",  "https://www.bjwork.xyz/h5/news/detail?id=xuzhi")
+                        .navigation(mContext);
+
+                break;
+
             case R.id.ll_zhize:
 
                 ARouter.getInstance().build("/web/Html5Activity")
